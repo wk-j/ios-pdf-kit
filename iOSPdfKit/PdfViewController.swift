8a1a4789b67ad1f7620ad39117e0b2b3c4543e84
wk-j <somnuk.wk@gmail.com>
2019-09-10 18:03:47 +0700

    Write file

--- iOSPdfKit/PdfViewController.swift
+++ iOSPdfKit/PdfViewController.swift
@@ -12,32 +12,78 @@ import PDFKit
 
 
 class PdfView: UIView {
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
-    override init(frame : CGRect) {
-        //super.init(frame : frame)
-        super.init(frame: frame)
+    init(frame : CGRect, pdfDocument: PDFDocument) {
+        super.init(frame : frame)
+        let pdfView = PDFView(frame: frame)
+        pdfView.document = pdfDocument
+        addSubview(pdfView)
+        
+    }
+}
+
+
+
+class PdfViewController: UIViewController {
+    var document: PDFDocument?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
         let url = Bundle.main.url(forResource: "tesla", withExtension: "pdf")
         guard let x = url else {
             return;
         }
         
-        let pdfView = PDFView(frame: frame)
-        let pdfDocument = PDFDocument(url: x)
-        pdfView.document = pdfDocument
-        addSubview(pdfView)
-    }
-}
-
-
-class PdfViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let pdf = PdfView(frame: UIScreen.main.bounds)
+        let document = PDFDocument(url: x)
+        
+        let pdf = PdfView(frame: UIScreen.main.bounds, pdfDocument: document!)
         self.view.addSubview(pdf)
+        
+        self.document = document
+        
+        setupButton()
+    }
+    
+    func setupButton() {
+        let button = UIButton()
+        button.frame = CGRect(x: self.view.frame.size.width - 60, y: 60, width: 50, height: 50)
+        button.backgroundColor = UIColor.red
+        button.setTitle("Name your Button ", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        self.view.addSubview(button)
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        show()
+    }
+    
+    func show() {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let logsPath = paths[0].appendingPathComponent("temp")
+        do {
+            try FileManager.default.createDirectory(at: logsPath, withIntermediateDirectories: true, attributes: nil)
+        } catch let error as NSError {
+            NSLog("Unable to create directory \(error.debugDescription)")
+        }
+        
+        let source = document!.documentURL!
+        let dest = logsPath.appendingPathComponent("xyz.pdf")
+        
+        do {
+            
+            try FileManager.default.copyItem(at: source, to: dest)
+            let activityVC = UIActivityViewController(activityItems: [dest], applicationActivities: nil)
+            self.present(activityVC, animated: true, completion: nil)
+        }
+        catch {
+            
+        }
     }
 }
 
